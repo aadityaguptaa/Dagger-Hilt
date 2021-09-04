98c01ca7acbe2a7f4120ca898a0daca93cb5ea2d
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2021-09-05 00:56:24 +0530

    Singleton

--- app/src/main/java/com/army/daggerhilttutorial/MainActivity.kt
+++ app/src/main/java/com/army/daggerhilttutorial/MainActivity.kt
@@ -2,8 +2,17 @@ package com.army.daggerhilttutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
+import javax.inject.Qualifier
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -17,7 +26,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println(someClass.doAThing())
-        println(someClass.doSomeOtherThing())
+        println(someClass.doAnotherThing())
+
 
     }
 }
@@ -25,27 +35,83 @@ class MainActivity : AppCompatActivity() {
 /**
  * This is a dependency. we inject this into the onCreate method using field injection
  * someOtherClass is a constructor injection
+ *
+ * Singleton makes the dependency alive as long as the Application is alive
  */
+@Singleton
 class SomeClass
 @Inject
 constructor(
-    private val someOtherClass: SomeOtherClass //constructor injection
+    @Impl1 private val someInterfaceImpl1: SomeInterface,
+    @Impl2 private val someInterfaceImpl2: SomeInterface //constructor injection
 ){
 
     fun doAThing(): String{
-        return "Look I did a thing!"
+        return someInterfaceImpl1.getAThing()
     }
 
-    fun doSomeOtherThing(): String{
-        return someOtherClass.doSomeOtherThing()
+    fun doAnotherThing(): String{
+        return someInterfaceImpl2.getAThing()
     }
+
 }
 
-class SomeOtherClass
+/**
+ * We can't directly inject a dependency, if we don't own the code.
+ * For example, in this case, we are injecting an interface, so hilt would think we don't own the code, so this won't work
+ */
+class SomeInterfaceImpl1
 @Inject
-constructor(){
+constructor(): SomeInterface{
 
-    fun doSomeOtherThing(): String{
-        return "Look I did some other thing!"
+    override fun getAThing(): String {
+        return "A Thing1"
     }
 }
+
+class SomeInterfaceImpl2
+@Inject
+constructor(): SomeInterface{
+
+    override fun getAThing(): String {
+        return "A Thing2"
+    }
+}
+
+interface  SomeInterface{
+
+    fun getAThing(): String
+}
+
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class MyModule{
+
+    /*@Singleton
+    @Binds
+    abstract fun bindSomeDependency(
+        someImpl: SomeInterfaceImpl
+    ): SomeInterface*/
+
+    @Impl1
+    @Singleton
+    @Provides
+    fun provideSomeInterface1(): SomeInterface{
+        return SomeInterfaceImpl1()
+    }
+
+    @Impl2
+    @Singleton
+    @Provides
+    fun provideSomeInterface2(): SomeInterface{
+        return SomeInterfaceImpl2()
+    }
+}
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Impl1
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Impl2
